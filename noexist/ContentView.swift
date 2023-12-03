e3f6862190a3da6378c395c3c595a12f5ff1023e
hongminpark <zhdals1026@gmail.com>
2023-12-03 17:09:08 +0900

    Add main animation data structure

--- noexist/ContentView.swift
+++ noexist/ContentView.swift
@@ -2,11 +2,7 @@
 
 import SwiftUI
 import SceneKit
-
-import SwiftUI
-import SceneKit
 import AVFoundation
-import SwiftVideoGenerator
 
 struct ContentView: View {
     let usdzFile = "Balenciaga_Defender"
@@ -22,24 +18,23 @@ struct ContentView: View {
             .frame(height: 300)
             .padding()
             .cornerRadius(12)
-            Button("Capture Snapshot") {
-                captureSnapshot = true
+            Button("play") {
+                sceneHolder.addRotationAnimation(x: 0, y: 0, z: CGFloat.pi * 2/4, duration: 2)
+                sceneHolder.applyAnimations()
             }
             .padding()
             .background(.black)
             .foregroundColor(.white)
-            Button(isCapturing ? "Stop" : "Record") {
-                if isCapturing {
-                    frameCaptureManager?.stopCapture()
-                    isCapturing = false
-                } else {
-                    isCapturing = true
-                    if let scnView = sceneHolder.scnView, let scene = scnView.scene {
-                        self.frameCaptureManager = FrameCaptureManager(scene: scene, view: scnView)
-                        self.frameCaptureManager?.startCapture()
-                    }
-                }
-                captureSnapshot = true
+            Button("move") {
+                sceneHolder.addRotationAnimation(x: 0, y: 0, z: CGFloat.pi * 2, duration: 10)
+                sceneHolder.updateAnimation(5.0)
+            }
+            .padding()
+            .background(.black)
+            .foregroundColor(.white)
+            Button("export") {
+                sceneHolder.addRotationAnimation(x: 0, y: 0, z: CGFloat.pi * 2/4, duration: 2)
+                sceneHolder.exportVideo()
             }
             .padding()
             .background(.black)
@@ -61,15 +56,8 @@ struct SceneKitView: UIViewRepresentable {
 
         if let url = Bundle.main.url(forResource: fileName, withExtension: "usdz"),
            let scene = try? SCNScene(url: url, options: nil) {
-            let rootNode = scene.rootNode
-
-            rootNode.eulerAngles = SCNVector3(0, 0, 0)
-            rootNode.position = SCNVector3(0, 0, 0)
-            rootNode.scale = SCNVector3(1, 1, 1)
-            let rotation = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat.pi * 2, duration: 10)
-            let repeatRotation = SCNAction.repeatForever(rotation)
-            rootNode.runAction(repeatRotation)
             sceneView.scene = scene
+            sceneHolder.scnView = sceneView
         }
         sceneHolder.scnView = sceneView
         return sceneView
@@ -84,106 +72,129 @@ struct SceneKitView: UIViewRepresentable {
             captureSnapshot = false
         }
     }
+    
+    static func rotate(for scnScene: SCNScene, x: CGFloat, y: CGFloat, z: CGFloat, duration: TimeInterval) {
+        let rootNode = scnScene.rootNode
+        rootNode.eulerAngles = SCNVector3(0, 0, 0)
+        rootNode.position = SCNVector3(0, 0, 0)
+        rootNode.scale = SCNVector3(1, 1, 1)
+        let rotation = SCNAction.rotateBy(x: x, y: y, z: z, duration: duration)
+        let repeatRotation = SCNAction.repeatForever(rotation)
+        rootNode.runAction(repeatRotation, forKey: "rotationKey")
+    }
 }
 
 class SceneHolder: ObservableObject {
-    var scnView: SCNView?
+    @Published var scnView: SCNView?
+    var animation: Animation?
+    @State private var frameCaptureManager: FrameCaptureManager?
+
+    init() {}
+    
+    func addRotationAnimation(x: CGFloat, y: CGFloat, z: CGFloat, duration: TimeInterval) {
+        let keyframes = [
+            Keyframe(time: 0, rotation: SCNVector4(0, 0, 0, 0)),
+            Keyframe(time: duration, rotation: SCNVector4(x, y, z, CGFloat.pi * 2))
+        ]
+
+        animation = Animation(keyframes: keyframes, duration: duration)
+    }
+    
+    func applyAnimations() {
+        guard let scnView = scnView, let rootNode = scnView.scene?.rootNode else { return }
+        let animator = Animator(node: rootNode)
+        animator.animation = self.animation
+        animator.applyAnimation()
+    }
+    
+    func updateAnimation(_ to: TimeInterval) {
+        guard let scnView = scnView, let rootNode = scnView.scene?.rootNode else { return }
+        let animator = Animator(node: rootNode)
+        animator.animation = self.animation
+        animator.updateAnimation(to: to)
+    }
+    
+    func exportVideo() {
+        guard let scnView = scnView, let scene = scnView.scene else { return }
+        let animator = Animator(node: scene.rootNode)
+        animator.animation = self.animation
+        let frameCaptureManager = FrameCaptureManager(scene: scene, view: scnView, animator: animator)
+        frameCaptureManager.startCapture()
+    }
 }
 
-class FrameCaptureManager {
-    private var scnRenderer: SCNRenderer
-    private var displayLink: CADisplayLink?
-    private var isCapturing = false
-    private var frameCount = 0
-    private var framesDirectory: URL?
+struct Keyframe {
+    var time: TimeInterval
+    var rotation: SCNVector4
+}
 
-    init(scene: SCNScene, view: SCNView) {
-        self.scnRenderer = SCNRenderer(device: nil, options: nil)
-        self.scnRenderer.scene = scene
-        self.scnRenderer.autoenablesDefaultLighting = true
-        self.scnRenderer.pointOfView = view.pointOfView
+struct Animation {
+    var keyframes: [Keyframe]
+    var duration: TimeInterval
+}
+
+class Animator {
+    var animation: Animation?
+    weak var node: SCNNode?
+
+    init(node: SCNNode) {
+        self.node = node
     }
 
-    func startCapture() {
-        isCapturing = true
-        displayLink = CADisplayLink(target: self, selector: #selector(renderAndCapture))
-        displayLink?.add(to: .current, forMode: .common)
-        createFramesDirectory()
-    }
-
-    func stopCapture() {
-        displayLink?.invalidate()
-        isCapturing = false
-    }
-
-    @objc private func renderAndCapture() {
-        if isCapturing {
-            let image = scnRenderer.snapshot(atTime: CACurrentMediaTime(), with: CGSize(width: 1080, height: 1080), antialiasingMode: .none)
-            if let data = image.pngData() {
-                saveFrame(data: data)
-            }
-            if frameCount == 60 {
-                generateVideoFromImages()
-                isCapturing = false
-            }
+    func applyAnimation() {
+        guard let animation = animation, let node = node else { return }
+        
+        let actions = animation.keyframes.map { keyframe -> SCNAction in
+            SCNAction.rotateTo(x: CGFloat(keyframe.rotation.x),
+                               y: CGFloat(keyframe.rotation.y),
+                               z: CGFloat(keyframe.rotation.z),
+                               duration: keyframe.time)
         }
+        
+        let sequence = SCNAction.sequence(actions)
+        node.runAction(sequence, forKey: "rotationAnimation")
     }
     
-    private func createFramesDirectory() {
-        let tempDir = FileManager.default.temporaryDirectory
-        let directoryName = UUID().uuidString
-        let directoryURL = tempDir.appendingPathComponent(directoryName)
-        do {
-            try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
-            framesDirectory = directoryURL
-        } catch {
-            print("Error creating frames directory: \(error)")
+    func updateAnimation(to time: TimeInterval) {
+        guard let animation = animation, let node = node else { return }
+
+        // Calculate the state of the animation at the given time
+        // This is a simplified example and assumes linear interpolation
+        if let firstKeyframe = animation.keyframes.first,
+           let lastKeyframe = animation.keyframes.last {
+            
+            let progress = min(max(time / animation.duration, 0), 1)
+            let startRotation = firstKeyframe.rotation
+            let endRotation = lastKeyframe.rotation
+            let interpolatedRotation = interpolate(start: startRotation, end: endRotation, progress: progress)
+            
+            node.eulerAngles = SCNVector3(interpolatedRotation.x, interpolatedRotation.y, interpolatedRotation.z)
         }
     }
 
-    private func saveFrame(data: Data) {
-        guard let directory = framesDirectory else { return }
+    private func interpolate(start: SCNVector4, end: SCNVector4, progress: CGFloat) -> SCNVector4 {
+        let floatProgress = Float(progress)  // Convert progress to Float
+
+        let deltaX = (end.x - start.x) * floatProgress
+        let deltaY = (end.y - start.y) * floatProgress
+        let deltaZ = (end.z - start.z) * floatProgress
+        let deltaW = (end.w - start.w) * floatProgress
+
+        let interpolatedX = start.x + deltaX
+        let interpolatedY = start.y + deltaY
+        let interpolatedZ = start.z + deltaZ
+        let interpolatedW = start.w + deltaW
+
+        return SCNVector4(interpolatedX, interpolatedY, interpolatedZ, interpolatedW)
+    }
+
+    func play() {
+        // Ensure the node is at the starting position
+        node?.eulerAngles = SCNVector3(0, 0, 0)
         
-        let fileName = String(format: "frame_%04d.png", frameCount)
-        let fileURL = directory.appendingPathComponent(fileName)
-        
-        do {
-            try data.write(to: fileURL)
-            frameCount += 1
-        } catch {
-            print("Error saving frame: \(error)")
-        }
+        // Apply the animation
+        applyAnimation()
     }
-    
-    func loadImagesFromDirectory(at path: URL) -> [UIImage] {
-        do {
-            let fileManager = FileManager.default
-            let contents = try fileManager.contentsOfDirectory(at: path, includingPropertiesForKeys: nil, options: [])
-            let imageFiles = contents.filter { $0.pathExtension == "png" }.sorted(by: { $0.lastPathComponent < $1.lastPathComponent })
-            imageFiles.forEach { fileURL in
-                print("Filename: \(fileURL.lastPathComponent)")
-            }
-            let images = imageFiles.compactMap { UIImage(contentsOfFile: $0.path) }
-            return images
-        } catch {
-            print("Error reading contents of directory: \(error)")
-            return []
-        }
-    }
-
-    func generateVideoFromImages() {
-        let images: [UIImage] = loadImagesFromDirectory(at: framesDirectory!)
-
-        VideoGenerator.fileName = "outputVideo"
-        VideoGenerator.videoDurationInSeconds = 1
-
-        VideoGenerator.current.generate(withImages: images, andAudios: [], andType: .multiple, { (progress) in
-          print(progress)
-        }, outcome: { (url) in
-          print(url)
-        })
-    }
-
 }
 
 #Preview {
